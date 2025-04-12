7e8252fdb240050bb1c4f6e666790ad34dafc777
Евгений Страхов <goodwin.s@mail.ru>
2025-04-13 00:41:37 +0300

    hw

--- src/main/java/pro/sky/hogwarts/controller/FacultyController.java
+++ src/main/java/pro/sky/hogwarts/controller/FacultyController.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pro.sky.hogwarts.model.Faculty;
-import pro.sky.hogwarts.model.Students;
 import pro.sky.hogwarts.service.FacultyService;
 
 import java.util.Collection;
