6872560ef46f57ff968315ebd03341d7140ae56c
Евгений Страхов <goodwin.s@mail.ru>
2025-04-13 01:47:25 +0300

    hw

---
+++ src/main/java/pro/sky/hogwarts/service/StudentService.java
@@ -0,0 +1,26 @@
+package pro.sky.hogwarts.service;
+
+import pro.sky.hogwarts.model.Faculty;
+import pro.sky.hogwarts.model.Students;
+
+import java.util.Collection;
+
+public interface StudentService {
+    Students createStudent(Students students);
+
+    Students editStudent(Students students);
+
+    Students getStudentById(Long id);
+
+    void deleteStudentById(Long id);
+
+    Collection<Students> getAllStudents();
+
+    Collection<Students> findByAge(int age);
+
+    Collection<Students> findByAgeBetween(Integer minAge, Integer maxAge);
+
+    Faculty getFaculty(Long id);
+
+    Collection<Students> getStudentsByFacultyId(Long id);
+}
