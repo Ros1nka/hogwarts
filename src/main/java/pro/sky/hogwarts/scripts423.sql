4da4350dde32639afe09ae0199fbe5378ff11842
Евгений Страхов <goodwin.s@mail.ru>
2025-05-13 21:24:42 +0300

    hw

--- src/main/java/pro/sky/hogwarts/scripts423.sql
+++
@@ -1,7 +0,0 @@
-SELECT students.name, students.age, faculty.name AS faculty_name
-FROM students
-INNER JOIN faculty ON students.faculty_id = faculty.id;
-
-SELECT s.name, s.age
-FROM students s
-INNER JOIN avatar a ON s.id = a.student_id;
