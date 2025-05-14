b37b1bcc1b718b3db22d78d167646124ca144d09
Евгений Страхов <goodwin.s@mail.ru>
2025-05-14 22:48:32 +0300

    hw

--- src/main/resources/liquibase/scripts/course-four-lesson-three.sql
+++ src/main/resources/liquibase/scripts/course-four-lesson-three.sql
@@ -1,4 +1,5 @@
 -- liquibase formatted sql
 
 -- changeset estrakhov:1
-CREEATE INDEX
+CREATE INDEX students_name_index ON students (name);
+CREATE INDEX faculty_name_color_index ON faculty (name, color);
