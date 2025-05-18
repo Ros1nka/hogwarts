3c5e01a9d21e8ebb3af9bbc503742f001ee272f7
Евгений Страхов <goodwin.s@mail.ru>
2025-05-18 15:36:38 +0300

    hw

--- src/main/java/pro/sky/hogwarts/controller/InfoController.java
+++ src/main/java/pro/sky/hogwarts/controller/InfoController.java
@@ -1,19 +1,24 @@
 package pro.sky.hogwarts.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile("!test")
+//@Profile("!test")
 @RestController
 public class InfoController {
+
+    Logger logger = LoggerFactory.getLogger(InfoController.class);
 
     @Value("${server.port}")
     private String port;
 
     @GetMapping("/port")
     public String getPort(){
-        return port + " - Port";
+        logger.debug("Was colling test port");
+        return port;
     }
 }
