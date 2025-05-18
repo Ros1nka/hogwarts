07060b94da321d6920ac87930c09a7ae9de63296
Евгений Страхов <goodwin.s@mail.ru>
2025-05-18 15:36:51 +0300

    hw

--- src/main/java/pro/sky/hogwarts/controller/InfoController.java
+++ src/main/java/pro/sky/hogwarts/controller/InfoController.java
@@ -3,11 +3,9 @@ package pro.sky.hogwarts.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@Profile("!test")
 @RestController
 public class InfoController {
 
